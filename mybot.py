2f7d0b813483a7c647da67de3d1039fca85a5e1a
Thomas Ashish Cherian <reach2ashish@users.noreply.github.com>
2016-08-26 20:54:03 +0530

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -1,27 +1,27 @@
 import telebot
-bot = telebot.TeleBot("myapikey")
+bot = telebot.TeleBot("MYKEY")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     print("Hello There")
-    bot.reply(message,"Hello There")
+    bot.reply_to(message,"Hello There")
 
 """
 normal message handling  // Commented out
 @bot.message_handler(func=lambda message: True)
 def send_welcome(message):
-    bot.reply(message,message.text)
+    bot.reply_to(message,message.text)
 
 """
 #To check the spelling
 @bot.message_handler(func=lambda message: True)
 def send_welcome(message):
-    d = open('diction.txt')
+    d = open('diction.dictionary')
     dictionaryItems = d.read().split("\n")
     if message.text in dictionaryItems:
-      bot.reply(message,"The word exists")
+      bot.reply_to(message,"The word exists")
     else:
-      bot.reply(message,"The word illae")
+      bot.reply_to(message,"The word illae")
 
 
 bot.polling()
